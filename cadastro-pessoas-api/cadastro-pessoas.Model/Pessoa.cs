063a1c444db61475ab1755c8d464bcf13cf98abf
rbrito <rbrito@IMPWS00396>
2022-01-29 19:38:58 -0300

    adicionadas propriedades pessoa

--- cadastro-pessoas-api/cadastro-pessoas.Model/Pessoa.cs
+++ cadastro-pessoas-api/cadastro-pessoas.Model/Pessoa.cs
@@ -8,5 +8,15 @@ namespace cadastro_pessoas.Model
 {
     public class Pessoa
     {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Nacionalidade { get; set; }
+        public long CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Logradouro { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
     }
 }
