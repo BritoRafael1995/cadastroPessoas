2576bed24e28023439168422e7a74e676397125e
rbrito <rbrito@IMPWS00396>
2022-01-29 20:02:55 -0300

    migrado para banco de dados local

--- cadastro-pessoas-api/cadastro-pessoas.Repository/Conexao/Contexto.cs
+++ cadastro-pessoas-api/cadastro-pessoas.Repository/Conexao/Contexto.cs
@@ -10,16 +10,14 @@ namespace cadastro_pessoas.Repository.Conexao
 {
     public class Contexto
     {
-        static readonly string stringConexao = "mongodb+srv://cad-pessoas:uX8r1dgmpfKGgYhd@rafaelprojects.nw03d.gcp.mongodb.net";
+        static readonly string stringConexao = "mongodb://localhost:27017";
         
         private static readonly MongoClient conexao = new MongoClient(stringConexao);
 
         public static IMongoDatabase Database = conexao.GetDatabase("CadastroPessoas");
-
-        //public static IMongoDatabase Database = conexao.GetDatabase("ProadvDocs");
         public static IMongoCollection<Pessoa> _collectionPessoa
         {
-            get => Database.GetCollection<Pessoa>("usuario");
+            get => Database.GetCollection<Pessoa>("pessoa");
         }
     }
 }
