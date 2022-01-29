85119f86fb51068376037e835382f7fb18c3a005
rbrito <rbrito@IMPWS00396>
2022-01-29 19:07:11 -0300

    criados repositorios

---
+++ cadastro-pessoas-api/cadastro-pessoas.Repository/Implementation/PessoasRepository.cs
@@ -0,0 +1,13 @@
+﻿using cadastro_pessoas.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadastro_pessoas.Repository.Implementation
+{
+    public class PessoasRepository : IPessoasRepository
+    {
+    }
+}
---
+++ cadastro-pessoas-api/cadastro-pessoas.Repository/Interface/IPessoasRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadastro_pessoas.Repository.Interface
+{
+    public interface IPessoasRepository
+    {
+    }
+}
