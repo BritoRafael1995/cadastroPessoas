762b763ccd1152979dac65a88035a540e05a2923
rbrito <rbrito@IMPWS00396>
2022-01-29 18:57:58 -0300

    criadas interfaces e implementação business

---
+++ cadastro-pessoas-api/cadastro-pessoas.Business/Implementation/PessoasBusiness.cs
@@ -0,0 +1,13 @@
+﻿using cadastro_pessoas.Business.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadastro_pessoas.Business.Implementation
+{
+    public class PessoasBusiness : IPessoasBusiness
+    {
+    }
+}
