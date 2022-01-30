6b774cd8cc2d722a0175d766199ff880c2caaf1b
rbrito <rbrito@IMPWS00396>
2022-01-30 19:35:04 -0300

    configurada rota

--- cadastro-pessoas-web/src/app/listar-pessoas/listar-pessoas-routing.module.ts
+++ cadastro-pessoas-web/src/app/listar-pessoas/listar-pessoas-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarPessoasComponent } from './listar-pessoas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListarPessoasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
